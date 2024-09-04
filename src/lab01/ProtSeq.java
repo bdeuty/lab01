8d0f0dc5d675eaa82b54e646a3b1c086f1848583
Bryce <Bryce@10.97.233.61>
2024-09-04 13:56:07 -0400

    9/4

--- src/lab01/ProtSeq.java
+++ src/lab01/ProtSeq.java
@@ -1,5 +1,32 @@
 package lab01;
 
-public class ProtSeq {
+import java.util.Random;
+
+public class ProtSeq 
+{
+	public static void main(String[] args)
+	{
+		//Redo of modified frequencies
+		String dna = "";
+		Random random = new Random();
+		float f = random.nextFloat();
+		int AAA_count = 0;
+		char[] dnachar = {'A','C','G','T'};
+		float ranges[] = new float[] {0.12f,0.50f,0.89f};
+		
+		for ( int x = 0; x < 1000; x++)
+		{
+			dna = "";
+			for ( int y = 0; y < 3; y++)
+			{
+				
+			}
+			
+			if (dna.equals("AAA"))
+			{
+				AAA_count++;
+			}
+		}
+	}
 
 }
